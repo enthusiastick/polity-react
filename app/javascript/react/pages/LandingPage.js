22547a69888469cb1a42ac54fdfa81e70f6fe012
Eben <eben.lowe@gmail.com>
2017-07-26 18:29:05 -0400

    Begin re-building landing page in React.js.

---
+++ app/javascript/react/pages/LandingPage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const LandingPage = () => {
+  return(
+    <div className="row">
+      <div className="small-12 columns">
+        <h1 className="text-center top-padded">
+          Polity
+        </h1>
+        <div className="callout">
+          <p>
+            <strong>Polity</strong> is the single sign-on provider for all apps hosted at enthusiastick.io &ndash; your one user account here at <strong>Polity</strong> is used to authenticate you to sites, experiments and web-toys found throughout the domain.
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default LandingPage
