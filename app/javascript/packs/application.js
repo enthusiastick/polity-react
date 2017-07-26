e9a5cf44b3380e669460da6de2c5a71f095c3a40
Eben <eben.lowe@gmail.com>
2017-07-26 19:23:54 -0400

    Add react-router and redux.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,14 +1,35 @@
+import 'whatwg-fetch';
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import { Route, Switch } from 'react-router';
+import createBrowserHistory from 'history/createBrowserHistory'
+import { BrowserRouter } from 'react-router-dom'
+import { syncHistoryWithStore, routerMiddleware, routerReducer } from 'react-router-redux'
+import { reducer as formReducer } from 'redux-form'
+import thunkMiddleware from 'redux-thunk';
 
 import LandingPage from '../react/pages/LandingPage';
+
+const store = createStore(
+  combineReducers({
+    form: formReducer,
+    routing: routerReducer
+  }),
+  applyMiddleware(thunkMiddleware, routerMiddleware)
+)
 
 document.addEventListener('DOMContentLoaded', () => {
   let reactElement = document.getElementById('react-app');
 
   if (reactElement) {
     ReactDOM.render(
-      <LandingPage />,
+      <Provider store={store}>
+        <BrowserRouter history={history}>
+          <Route exactpath='/' component={LandingPage} />
+        </BrowserRouter>
+      </Provider>,
       reactElement
     )
   }
